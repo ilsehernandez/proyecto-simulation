66542f6aaa220adfbe52ff12238011b0d7625de1
Ilse Hernandez <serenilka@hotmail.com>
2020-04-20 00:52:06 -0600

    Updates

---
+++ precache-manifest.de46648f20301c964bd0ca3c5597c813.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb2c50fee3cf43990340891b9ebd631",
+    "url": "/proyecto-simulation/index.html"
+  },
+  {
+    "revision": "b1c8d41541f1fc3eabdb",
+    "url": "/proyecto-simulation/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "143bc72a317ee1b1f1e5",
+    "url": "/proyecto-simulation/static/css/main.95ab5cf1.chunk.css"
+  },
+  {
+    "revision": "b1c8d41541f1fc3eabdb",
+    "url": "/proyecto-simulation/static/js/2.33ed23ba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/proyecto-simulation/static/js/2.33ed23ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143bc72a317ee1b1f1e5",
+    "url": "/proyecto-simulation/static/js/main.35c92436.chunk.js"
+  },
+  {
+    "revision": "0e3bffcbfbb265ecf7cc",
+    "url": "/proyecto-simulation/static/js/runtime-main.9835110f.js"
+  }
+]);
